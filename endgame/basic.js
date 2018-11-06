4e39729dac2492f6fcc56befa748858b44fed93d
Bradley Harris <bradleykh@gmail.com>
2018-11-06 08:15:46 -0600

    Improved training interface

--- endgame/basic.js
+++ endgame/basic.js
@@ -1,6 +1,5 @@
-
 /*
-Pawn endgames
+Basic endgames
 
 Each exercise should have:
 - a prompt (e.g. Black to move, White to move a draw/win)
@@ -8,17 +7,10 @@ Each exercise should have:
 - a solution (a sequence of moves)
 */
 
-
-const test1 = [
-    'Black to move',
-    '3k4/8/8/8/3PK3/8/8/8 b - - 0 1',
-    'Ke8 Ke5 Ke7 d5 Kd7 d6 Kd8 Ke6 Ke8 d7+ Kd8'    
-];
-
-const test2 = [
+const basic1 = [
     'Black to move and draw',
     '3k4/8/8/8/3PK3/8/8/8 b - - 0 1',
     'Ke8 Ke5 Ke7 d5 Kd7 d6 Kd8 Ke6 Ke8 d7+ Kd8'    
 ];
 
-const pawnEndgames = [test1, test2];
+const basicEndgames = [basic1];
---
+++ endgame/pawn.js
@@ -0,0 +1,17 @@
+
+/*
+Pawn endgames
+
+Each exercise should have:
+- a prompt (e.g. Black to move, White to move a draw/win)
+- a starting FEN position (which includes the turn)
+- a solution (a sequence of moves)
+*/
+
+const test1 = [
+    'Black to move',
+    '3k4/8/8/8/3PK3/8/8/8 b - - 0 1',
+    'Ke8 Ke5 Ke7 d5 Kd7 d6 Kd8 Ke6 Ke8 d7+ Kd8'    
+];
+
+const pawnEndgames = [test1];
